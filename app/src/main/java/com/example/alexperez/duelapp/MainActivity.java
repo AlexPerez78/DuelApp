a4679469f5d38c187e980dc8daed38eeb8068735
Alex Perez <alex_perez25@hotmail.com>
2017-10-11 13:15:11 -0700

    Intent Activities Added
    Added Base code needed to head to new page

--- app/src/main/java/com/example/alexperez/duelapp/MainActivity.java
+++ app/src/main/java/com/example/alexperez/duelapp/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.alexperez.duelapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -23,6 +26,35 @@ public class MainActivity extends AppCompatActivity {
         mToggle.syncState();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        NavigationView mNavigationView = (NavigationView) findViewById(R.id.nav_menu);
+            mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch(item.getItemId()){
+                        case(R.id.nav_account):
+                            Intent accountActivity = new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(accountActivity);
+                            break;
+
+                        case(R.id.nav_banlist):
+                            Intent banlistActivity = new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(banlistActivity);
+                            break;
+
+                        case(R.id.nav_tournament):
+                            Intent tournamentActivity = new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(tournamentActivity);
+                            break;
+
+                        case(R.id.nav_settings):
+                            Intent settingsActivity = new Intent(getApplicationContext(),MainActivity.class);
+                            startActivity(settingsActivity);
+                            break;
+                    }
+                    return true;
+                }
+            });
     }
 
     @Override
