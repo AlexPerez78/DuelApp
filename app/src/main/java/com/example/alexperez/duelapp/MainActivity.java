0b2306d88b2931f6da9e4b1c9de8689b72fd8bf9
Alex Perez <alex_perez25@hotmail.com>
2017-11-02 13:35:58 -0700

    BottomNavigationBar Setup + Sounds Added

--- app/src/main/java/com/example/alexperez/duelapp/MainActivity.java
+++ app/src/main/java/com/example/alexperez/duelapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.alexperez.duelapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
@@ -10,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
+import java.util.Random;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
@@ -80,11 +84,50 @@ public class MainActivity extends AppCompatActivity {
     * BottomNavigationView setup
     */
     private void setupBottomNavigationView(){
-        //Log.d("setupBottomNavigationView", "Setting up BottomNavigationView");
         BottomNavigationViewEx bottomNavigationViewEx = (BottomNavigationViewEx)findViewById(R.id.bottomNavigationViewBar);
         bottomNavigationViewEx.enableAnimation(true);
         bottomNavigationViewEx.enableItemShiftingMode(false);
         bottomNavigationViewEx.enableShiftingMode(false);
         bottomNavigationViewEx.setTextVisibility(false);
+        enableNavigation(MainActivity.this, bottomNavigationViewEx);
+    }
+
+    public static void enableNavigation(final Context context, BottomNavigationViewEx view){
+        view.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.dice:
+                        int diceRolled = (int) (Math.random() * 6) + 1;
+                        MediaPlayer mediaPlayer = MediaPlayer.create(context, R.raw.diceroll);
+                        mediaPlayer.start();
+                        Toast.makeText(context, "You Rolled a " + diceRolled , Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.coin:
+                        Random randomNum = new Random();
+                        int coinFlip = randomNum.nextInt(2);
+                        String output;
+                        if(coinFlip==0){
+                            output = "Heads";
+                        }
+                        else{
+                            output = "Tails";
+                        }
+                        MediaPlayer mediaPlayerCoin = MediaPlayer.create(context, R.raw.coinflipfx);
+                        mediaPlayerCoin.start();
+                        Toast.makeText(context, "You Flipped " + output , Toast.LENGTH_SHORT).show();
+                        break;
+
+                    case R.id.nightmode:
+                        break;
+
+                    case R.id.reset:
+                        break;
+
+                }
+                return false;
+            }
+        });
     }
 }
