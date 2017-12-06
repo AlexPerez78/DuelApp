f252a8f074901057bce590cf9614d76a458809be
Alex Perez <alex_perez25@hotmail.com>
2017-12-06 14:49:43 -0800

    Dice Roll Recursion
    .Added Recursion for Dice roll to mimic a "Dice Roll" without 3D
    animations
    .Addded Table Image to set as Background for Dice Roll and Coin Flip
    Background to mimic the action on a table.

--- app/src/main/java/com/example/alexperez/duelapp/DiceRoll.java
+++ app/src/main/java/com/example/alexperez/duelapp/DiceRoll.java
@@ -1,7 +1,10 @@
 package com.example.alexperez.duelapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -9,9 +12,11 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -19,11 +24,14 @@ public class DiceRoll extends Activity {
     private Random random = new Random();
     private HashMap<Integer, Integer> diceResource = new HashMap<>();
     TextView btn_close;
+    int dice_Spinning = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dice_roll);
+
+        ContextCompat.getDrawable(this,R.drawable.table);
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -37,6 +45,8 @@ public class DiceRoll extends Activity {
         params.gravity = Gravity.CENTER;
         params.x = 0;
         params.y = -20;
+
+
 
         getWindow().setAttributes(params);
 
@@ -61,12 +71,39 @@ public class DiceRoll extends Activity {
         starter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int diceRolled = (int) (Math.random() * 6) + 1;
+                /*int diceRolled = (int) (Math.random() * 6) + 1;
                 MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.diceroll);
                 mediaPlayer.start();
-                starter.setImageResource(diceResource.get(diceRolled));
-                Toast.makeText(getApplicationContext(),"You Rolled A " + diceRolled, Toast.LENGTH_SHORT).show();
+                starter.setImageResource(diceResource.get(diceRolled));*/
+                DiceRoll();
             }
         });
+    }
+
+    public void DiceRoll(){
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                int diceRolled = (int) (Math.random() * 6) + 1;
+                ImageView starter = (ImageView)findViewById(R.id.starter);
+                //MediaPlayer mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.diceroll);
+
+                starter.setEnabled(false);
+                //mediaPlayer.start();
+                starter.setImageResource(diceResource.get(diceRolled));
+                dice_Spinning++;
+                //mediaPlayer.release();
+
+                if(dice_Spinning < 10){
+                    DiceRoll(); //Recursion to Mimic a Dice Rolling
+
+                }else{
+                    starter.setEnabled(true);
+                    dice_Spinning = 0; //Reset Number of Rolls
+                    Toast.makeText(getApplicationContext(),"You Rolled A " + diceRolled, Toast.LENGTH_SHORT).show();
+                }
+            }
+            //}
+        }, 100);
     }
 }
