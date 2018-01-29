4b1db09d5c672891374ecc475753eb208ca14789
Alex Perez <alex_perez25@hotmail.com>
2018-01-29 00:00:35 -0800

    Main Page
    Finished adding everything
    1) Center the Piece using Contraints
    2) Add Calc to increase/Decrease LP

--- app/src/main/java/com/example/alexperez/duelapp/MainActivity.java
+++ app/src/main/java/com/example/alexperez/duelapp/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.alexperez.duelapp;
 
-import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -14,16 +13,21 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
-
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
+
+    static ProgressBar progressBar_Player1,progressBar_Player2;
+    static TextView healthValue_Player1, healthValue_Player2;
 
     private static int ACTIVITY_NUM = 0;
 
@@ -31,6 +35,73 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MediaPlayer life_Point_loss = MediaPlayer.create(getApplicationContext(), R.raw.lifepoint_drop);
+
+        /*-------------------------------------Player 1 Controls-------------------------------------------*/
+        progressBar_Player1 = (ProgressBar) findViewById(R.id.progressBar_PLayer1);
+        progressBar_Player1.setMax(8000);
+        progressBar_Player1.setProgress(8000);
+
+        healthValue_Player1 = (TextView) findViewById(R.id.healthpoint_Value_Player1);
+        healthValue_Player1.setText(Integer.toString(progressBar_Player1.getProgress()));
+
+        Button player1_Add = (Button)findViewById(R.id.add_LP_Player1);
+        player1_Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar_Player1.setProgress(8000);
+                healthValue_Player1.setText(Integer.toString(progressBar_Player1.getProgress()));
+
+                if(progressBar_Player1.getProgress() > 8000){
+                    //Set new Max with progressBar current Lifepoint
+                    progressBar_Player1.setMax(progressBar_Player1.getProgress());
+                }
+            }
+        });
+
+        Button player1_Sub = (Button)findViewById(R.id.sub_LP_Player1);
+        player1_Sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                life_Point_loss.start();
+                progressBar_Player1.setProgress(4000);
+                healthValue_Player1.setText(Integer.toString(progressBar_Player1.getProgress()));
+            }
+        });
+    /*-------------------------------------Player 2 Controls-------------------------------------------*/
+        progressBar_Player2 = (ProgressBar) findViewById(R.id.progressBar_PLayer2);
+        progressBar_Player2.setMax(8000);
+        progressBar_Player2.setProgress(8000);
+
+        healthValue_Player2 = (TextView) findViewById(R.id.healthpoint_Value_Player2);
+        healthValue_Player2.setText(Integer.toString(progressBar_Player2.getProgress()));
+
+        Button player2_Add = (Button)findViewById(R.id.add_LP_Player2);
+        player2_Add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                progressBar_Player2.setProgress(8000);
+                healthValue_Player2.setText(Integer.toString(progressBar_Player2.getProgress()));
+
+                if(progressBar_Player2.getProgress() > 8000){
+                    //Set new Max with progressBar current Lifepoint
+                    progressBar_Player2.setMax(progressBar_Player2.getProgress());
+                }
+            }
+        });
+
+        Button player2_Sub = (Button)findViewById(R.id.sub_LP_Player2);
+        player2_Sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                life_Point_loss.start();
+                progressBar_Player2.setProgress(4000);
+                healthValue_Player2.setText(Integer.toString(progressBar_Player2.getProgress()));
+            }
+        });
+
+
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open, R.string.close);
@@ -128,6 +199,12 @@ public class MainActivity extends AppCompatActivity {
                                 .setPositiveButton("Restart", new DialogInterface.OnClickListener() {
                                     public void onClick(DialogInterface dialog, int id) {
                                         Toast.makeText(context, "Game Has Been Resetted " , Toast.LENGTH_SHORT).show();
+                                        progressBar_Player1.setProgress(8000); //Reset The Game
+                                        healthValue_Player1.setText(Integer.toString(progressBar_Player1.getProgress()));
+
+                                        progressBar_Player2.setProgress(8000); //Reset The Game
+                                        healthValue_Player2.setText(Integer.toString(progressBar_Player2.getProgress()));
+
                                     }
                                 })
                                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
